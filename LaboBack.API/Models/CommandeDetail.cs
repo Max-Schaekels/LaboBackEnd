49784ca9e37a999a1ce49fd9394365a1da1f4f3d
Maximilien Schaekels <m.schaekels@stag.technofuturtic.education>
2025-06-17 09:52:09 +0200

    modification de CommandeDetail

--- LaboBack.API/Models/CommandeDetail.cs
+++ LaboBack.API/Models/CommandeDetail.cs
@@ -1,4 +1,6 @@
-﻿namespace LaboBack.API.Models
+﻿using System.Text.Json.Serialization;
+
+namespace LaboBack.API.Models
 {
     public class CommandeDetail
     {
@@ -8,6 +10,7 @@
         public string Categorie { get; set; }
         public decimal PrixHTVA { get; set; }
         public decimal PrixTVAC { get; set; }
+        [JsonPropertyName("quantite")]
         public int QuantiteCommandee { get; set; }
     }
 }
